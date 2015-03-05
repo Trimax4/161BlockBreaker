321daedfb5feb7724f2d5b723e4f5882c4210646
Hao Dinh <hqdinh@uci.edu>
2015-03-04 16:04:35 -0800

    Added UI class. Also added my main.cpp which loads in button textures among other things.

---
+++ Blockbreaker/UI.cpp
@@ -0,0 +1,290 @@
+#include "UI.h"
+#include <iostream>
+#include <string>
+
+
+/*
+Creates a UI object that has frames with
+width and height equal to the inputted values
+@param width The desired width of each frame
+@param height The desired height of each frame
+@render The renderer to draw the UI with
+@return The UI object
+*/
+UI::UI(SDL_Renderer* ren)
+{
+	renderer = ren;
+}
+
+UI::~UI()
+{
+	TTF_CloseFont(font);
+}
+
+
+/*
+Set the position of the given element (top-left corner)
+@param x The new x position
+@param y The new y position
+*/
+void UI::setPos(std::string element, int x, int y)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	buttonList[element].x = x;
+	buttonList[element].y = y;
+}
+
+
+/*
+Increment/decrement the x position of the given element by delta
+@param delta The amount to increment/decrement x by
+*/
+void UI::movex(std::string element, int delta)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	buttonList[element].x += delta;
+}
+
+/*
+Increment/decrement the y position of the given element by delta
+@param delta The amount to increment/decrement y by
+*/
+void UI::movey(std::string element, int delta)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	buttonList[element].y += delta;
+}
+
+/*
+Get the current x position of the given element
+@return Current x position of given element
+*/
+int UI::getX(std::string element)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return -1;
+	return buttonList[element].x;
+}
+
+/*
+Get the current y position of the UI
+@return the current y position of the UI
+*/
+int UI::getY(std::string element)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return -1;
+	return buttonList[element].y;
+}
+
+/*
+Creates a button for the UI
+@param name The name for the button
+@param idle_frame The source of the idle button frame
+@param push_frame The source of the pushed button frame
+@param x The x coordinate of the button's top left corner
+@param y The y coordinate of the button's top left corner
+*/
+void UI::makeButton(std::string name, SDL_Texture* idle_frame, SDL_Texture* push_frame, int x, int y)
+{
+	buttonList[name].x = x;
+	buttonList[name].y = y;
+	buttonList[name].idle_texture = idle_frame;
+	buttonList[name].active_texture = push_frame;
+	buttonList[name].angle = 0.0;
+	SDL_QueryTexture(idle_frame, NULL, NULL, &buttonList[name].width, &buttonList[name].height);
+	buttonList[name].hStretch = 0;
+	buttonList[name].wStretch = 0;
+
+}
+
+void UI::makeMenu(std::string name, SDL_Texture* idle_frame, int x, int y)
+{
+	buttonList[name].x = x;
+	buttonList[name].y = y;
+	buttonList[name].idle_texture = idle_frame;
+	buttonList[name].angle = 0.0;
+	SDL_QueryTexture(idle_frame, NULL, NULL, &buttonList[name].width, &buttonList[name].height);
+	buttonList[name].hStretch = 0;
+	buttonList[name].wStretch = 0;
+
+}
+
+/*
+Renders the specified element
+@param frameIndex The element to render
+*/
+void UI::show(std::string element)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	//if (isClicked) frameToShow = buttonlist active_texture
+	SDL_Rect destinationRect;
+	destinationRect.x = buttonList[element].x;
+	destinationRect.y = buttonList[element].y;
+	destinationRect.w = buttonList[element].width + buttonList[element].wStretch;
+	destinationRect.h = buttonList[element].height + buttonList[element].hStretch;
+	if (destinationRect.w <= 0)
+		destinationRect.w = 1;
+	if (destinationRect.h <= 0)
+		destinationRect.h = 1;
+	SDL_RenderCopyEx(renderer, buttonList[element].idle_texture, NULL, &destinationRect, buttonList[element].angle, NULL, buttonList[element].flip);
+}
+
+/*
+Sets the rotation of the given element to the given degrees
+@param new_angle The angle to set the given element to
+*/
+void UI::rotate(std::string element, double new_angle)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	double* angle = &buttonList[element].angle;
+	*angle = new_angle;
+	if (*angle < 0)
+	{
+		*angle = 360 + *angle;
+		while (*angle > 360)
+		{
+			*angle = *angle - 360;
+			return;
+		}
+	}
+	while (*angle > 360)
+		*angle = *angle - 360;
+}
+
+/*
+Horizontally flip the given element
+*/
+void UI::flip_horizontal(std::string element)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	switch (buttonList[element].flip)
+	{
+	case SDL_FLIP_NONE:
+		//std::cout << "Flipped from none" << std::endl;
+		buttonList[element].flip = SDL_FLIP_HORIZONTAL;
+		break;
+	case SDL_FLIP_HORIZONTAL:
+		//std::cout << "Reversed horizontal flip, no vertical" << std::endl;
+		buttonList[element].flip = SDL_FLIP_NONE;
+		break;
+	case SDL_FLIP_VERTICAL:
+		//std::cout << "Added horizontal flip to vertical flip" << std::endl;
+		buttonList[element].flip = (SDL_RendererFlip)(SDL_FLIP_VERTICAL | SDL_FLIP_HORIZONTAL);
+		break;
+	case SDL_FLIP_VERTICAL | SDL_FLIP_HORIZONTAL:
+		//std::cout << "Removed horizontal flip from vertical flip" << std::endl;
+		buttonList[element].flip = SDL_FLIP_VERTICAL;
+		break;
+	}
+}
+
+/*
+Horizontally flip the given element
+*/
+void UI::flip_vertical(std::string element)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	switch (buttonList[element].flip)
+	{
+	case SDL_FLIP_NONE:
+		//std::cout << "Flipped from none" << std::endl;
+		buttonList[element].flip = SDL_FLIP_VERTICAL;
+		break;
+	case SDL_FLIP_VERTICAL:
+		//std::cout << "Reversed vertical flip, no horizontal" << std::endl;
+		buttonList[element].flip = SDL_FLIP_NONE;
+		break;
+	case SDL_FLIP_HORIZONTAL:
+		//std::cout << "Added vertical flip to horizontal flip" << std::endl;
+		buttonList[element].flip = (SDL_RendererFlip)(SDL_FLIP_VERTICAL | SDL_FLIP_HORIZONTAL);
+		break;
+	case SDL_FLIP_VERTICAL | SDL_FLIP_HORIZONTAL:
+		//std::cout << "Removed vertical flip from horizontal flip" << std::endl;
+		buttonList[element].flip = SDL_FLIP_HORIZONTAL;
+		break;
+	}
+}
+
+/*
+Stretch the height and the width of the given element by the given values
+@param wStretch The amount to stretch the given element's width by
+@param hStretch The amount to stretch the given element's height by
+*/
+void UI::stretch(std::string element, int wStretch, int hStretch)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	buttonList[element].hStretch = hStretch;
+	buttonList[element].wStretch = wStretch;
+}
+
+/*
+Stretch the height and width of the given element by the given value
+@param stretch The amount to stretch the UI by
+*/
+void UI::stretch(std::string element, int stretch)
+{
+	if (buttonList.find(element) == buttonList.end())
+		return;
+	buttonList[element].hStretch = stretch;
+	buttonList[element].wStretch = stretch;
+}
+
+
+
+void UI::showText(std::string text, std::string &fontName, int fontSize, int x, int y, SDL_Renderer* renderer)
+{
+	if (TTF_Init() != 0){
+		std::cout << "TTF_Init" << std::endl;
+		SDL_Quit();
+		return;
+	}
+	font = TTF_OpenFont(fontName.c_str(), fontSize);
+	if (font == nullptr)
+	{
+		std::string error = SDL_GetError();
+		std::cout << error << std::endl;
+		TTF_CloseFont(font);
+		return;
+	}
+
+	SDL_Color color = { 255, 255, 255, 255 };
+
+	SDL_Surface* textSurface = TTF_RenderText_Blended(font, text.c_str(), color);
+	if (textSurface == nullptr)
+	{
+		std::cout << "Can't create surface" << std::endl;
+		return;
+	}
+
+	int text_width = textSurface->w;
+	int text_height = textSurface->h;
+
+
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = text_width;
+	dst.h = text_height;
+
+	SDL_Texture *texture_message = SDL_CreateTextureFromSurface(renderer, textSurface);
+	if (texture_message == nullptr)
+	{
+		std::cout << "Can't create texture" << std::endl;
+		return;
+	}
+
+	SDL_RenderCopy(renderer, texture_message, NULL, &dst);
+	SDL_FreeSurface(textSurface);
+	SDL_DestroyTexture(texture_message);
+
+}
+
---
+++ Blockbreaker/main.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <sstream>
+#include <Windows.h>
+#include <SDL.h>
+#include "res_path.h"
+#include "cleanup.h"
+#include "SDL_image.h"
+#include "UI.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+/**
+* Log an SDL error with some error message to the output stream of our choice
+* @param os The output stream to write the message to
+* @param msg The error message to write, format will be msg error: SDL_GetError()
+*/
+void logSDLError(std::ostream &os, const std::string &msg){
+	os << msg << " error: " << SDL_GetError() << std::endl;
+	std::ostringstream errMsg;
+	errMsg << " error: " << SDL_GetError() << std::endl;
+	OutputDebugString(errMsg.str().c_str());
+}
+
+/**
+* Loads an image into a texture on the rendering device
+* @param file The image file to load
+* @param ren The renderer to load the texture onto
+* @return the loaded texture, or nullptr if something went wrong.
+*/
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
+	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+	if (texture == nullptr){
+		logSDLError(std::cout, "LoadTexture");
+	}
+	return texture;
+}
+
+
+int main(int argc, char **argv){
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::ostringstream debugMsg;
+		debugMsg << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		OutputDebugString(debugMsg.str().c_str());
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG){
+		logSDLError(std::cout, "IMG_Init");
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Window *window = SDL_CreateWindow("UI Demo", 800, 100, SCREEN_WIDTH,
+		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr){
+		logSDLError(std::cout, "CreateWindow");
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr){
+		logSDLError(std::cout, "CreateRenderer");
+		cleanup(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	const std::string resPath = getResourcePath("UIDemo");
+
+	/*SDL_Texture* idle_button = loadTexture(resPath + "menu_button.png", renderer);
+	if (idle_button == nullptr)
+	{
+	cleanup(idle_button, renderer, window);
+	IMG_Quit();
+	SDL_Quit();
+	return 1;
+	}
+
+	SDL_Texture* pushed_button = loadTexture(resPath + "menu_button2.png", renderer);
+	if (pushed_button == nullptr)
+	{
+	cleanup(pushed_button, renderer, window);
+	IMG_Quit();
+	SDL_Quit();
+	return 1;
+	}*/
+	SDL_Texture* esc_menu = loadTexture(resPath + "menu.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(esc_menu, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* quit_button = loadTexture(resPath + "button_quit.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(quit_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* quit_button_pushed = loadTexture(resPath + "button_quit_pushed.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(quit_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* resume_button = loadTexture(resPath + "button_resume.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(resume_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* resume_button_pushed = loadTexture(resPath + "button_resume_pushed.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(resume_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* restart_button = loadTexture(resPath + "button_restart.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(restart_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Texture* restart_button_pushed = loadTexture(resPath + "button_restart_pushed.png", renderer);
+	if (esc_menu == nullptr)
+	{
+		cleanup(restart_button, renderer, window);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	UI* ui = new UI(renderer);
+
+	int x = SCREEN_WIDTH / 2;
+	int y = SCREEN_HEIGHT / 2;
+
+
+	ui->makeMenu("esc_menu", esc_menu, 220, 45);
+
+	//ui->makeButton("test_button", idle_button, pushed_button, x, y);
+	//ui->makeButton("second_button", pushed_button, idle_button, 50, 50);
+	ui->makeButton("quit_button", quit_button, quit_button_pushed, 240, 275);
+	ui->makeButton("restart_button", restart_button, restart_button_pushed, 240, 185);
+	ui->makeButton("resume_button", resume_button, resume_button_pushed, 240, 95);
+
+
+	//ui->showText("TEST", resPath + "BLOCKUP.ttf", 50, 0, 0, renderer);
+
+
+	SDL_Event e;
+	bool flag = false;
+	bool textflag = false;
+	bool quit = false;
+	while (!quit){
+		//frames =+ 1;
+		while (SDL_PollEvent(&e)){
+			if (e.type == SDL_QUIT){
+				quit = true;
+			}
+			if (e.type == SDL_KEYDOWN){
+
+				if (e.key.keysym.sym == SDLK_ESCAPE)
+					quit = true;
+				else if (e.key.keysym.sym == SDLK_q)
+				{
+					//would be best if we can add something here
+					//that can pause the game
+
+					flag = !flag;
+				}
+				else if (e.key.keysym.sym == SDLK_w)
+				{
+					textflag = !textflag;
+				}
+			}
+		}
+		//Render the scene
+
+		SDL_RenderClear(renderer);
+		if (flag == true)
+		{
+			ui->show("esc_menu");
+			ui->show("resume_button");
+			ui->show("restart_button");
+			ui->show("quit_button");
+		}
+		else if (textflag == true)
+		{
+			ui->showText("TEST", resPath + "BLOCKUP.ttf", 100, 0, 50, renderer);
+		}
+
+		ui->showText("TEST", resPath + "BLOCKUP.ttf", 50, 0, 0, renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	cleanup(esc_menu, quit_button, quit_button_pushed,
+		resume_button, resume_button_pushed,
+		restart_button, restart_button_pushed, renderer, window);
+
+	IMG_Quit();
+	SDL_Quit();
+
+	SDL_Quit();
+
+	return 0;
+}
