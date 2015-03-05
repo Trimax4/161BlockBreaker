7ddbd8829da33cdd247389a5c4aa33828a630042
cyrus tabatabai <ctabatab@uci.edu>
2015-03-05 14:29:24 -0800

    added to header file declarations for methods to change direction and speed in vector based motion system

--- Blockbreaker/Physics.h
+++ Blockbreaker/Physics.h
@@ -8,7 +8,10 @@ class Physic
    public:
       Physic(Entity ball);
       bool checkForCollision(std::vector<Entity> blocks, int nextX, int nextY);
-
+      void changeXDirection(double* x);
+      void changeYDirection(double* y);
+      void changeXSpeed(double* x);
+      void changeYSpeed(double* y);
 
    private:
       int ballWidth;
